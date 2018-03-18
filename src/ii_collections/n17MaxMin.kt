c27ad95ee308cf5217a973993f8c01aaca70a15c
au9ustine <duke.augustine@gmail.com>
2018-03-18 23:14:09 +0800

    Part II :: 19 Sum  :: Completed

--- src/ii_collections/n17MaxMin.kt
+++ src/ii_collections/n17MaxMin.kt
@@ -12,5 +12,5 @@ fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? {
 
 fun Customer.getMostExpensiveOrderedProduct(): Product? {
     // Return the most expensive product which has been ordered
-    return this.orders.map { it.products.maxBy { it.price } }.maxBy { it!!.price }
+    return this.orders.flatMap { it.products }.maxBy { it.price }
 }
